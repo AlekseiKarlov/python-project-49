87cd25b0ef4c3b261682d0dbd79634c87d2b9077
AlekseiKarlov <leha-steil@bk.ru>
2022-10-02 21:55:21 +0400

    6 задание часть

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ brain-games:
 brain-even:
 	poetry run brain-even
 
+brain-calc:
+	poetry run brain-calc
+
 build:
 	poetry build
 
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,46 @@
+from random import randint
+import prompt
+import random
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('What is the result of the expression?')
+    n = 0
+    znak = '+-*'
+    while (n < 4):
+        if (n == 3):
+            print(f'Congratulations, {name}!')
+            break
+        pznak = random.choice(znak)
+        randa = randint(0, 99)
+        randb = randint(0, 99)
+        print('Question:', randa, pznak, randb)
+        number = prompt.string('Your answer: ')
+        number = int(number)
+        if (pznak == '+' and (randa + randb == number)):
+            print('Correct!')
+            n += 1
+        elif (pznak == '-' and (randa - randb == number)):
+            n += 1
+            print('Correct!')
+        elif (pznak == '*' and (randa * randb == number)):
+            n += 1
+            print('Correct!')
+        else:
+            if (pznak == "+"):
+                print(f"'{number}' is wrong answer ;(. Correct answer was '{randa + randb}'.")
+            elif (pznak == "-"):
+                print(f"'{number}' is wrong answer ;(. Correct answer was '{randa - randb}'.")
+            elif (pznak == "*"):
+                print(f"'{number}' is wrong answer ;(. Correct answer was '{randa * randb}'.")
+            else:
+                print('Некорректный ввод')
+            break
+
+
+
+if __name__ == '__main__':
+    main()
